823a940fdddb847a8cb2ee88ea7923cd56a71ac5
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-09-14 16:48:29 -0400

    init

---
+++ src/modules/SideBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Paper } from '@material-ui/core';
+
+class SideBar extends Component {
+	state = {}
+	render() {
+		return (
+			<Paper>
+				<div> Hello! </div>
+			</Paper>
+		);
+	}
+}
+
+export default SideBar;
