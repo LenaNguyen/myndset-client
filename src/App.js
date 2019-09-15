83f29c6cfb3fcd6978f4f74a39b705c46880a9de
Ashley <ashlaaayy@hotmail.com>
2019-09-14 20:57:32 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SideBar from './modules/SideBar';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<p>HELLO!</p>
+			<SideBar />
 		</div>
 	);
 }
--- src/modules/SideBar.jsx
+++ src/modules/SideBar.jsx
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import { Paper } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import './SideBar.css';
+import { withStyles } from '@material-ui/core/styles';
+import todayicon from '../icons/today icon.svg';
+import weeklyicon from '../icons/weekly icon.svg';
+import monthlyicon from '../icons/monthly icon.svg';
+import yearlyicon from '../icons/yearly icon.svg';
+
+
+const styles = {
+	view_button: {
+		background: '#CBE0FF',
+		borderRadius: '5 px',
+		textAlign: 'left',
+		width: 'calc(100% - 40px)',
+	},
+
+}
+
 
 class SideBar extends Component {
 	state = {}
 	render() {
-		return (
-			<Paper>
-				<div> Hello! </div>
-			</Paper>
+		const { view_button } = this.props.classes;
+		return (			
+			<div className="sidebar-bg">  
+				<div className="hello"><h1>Hello, Alex!</h1></div>
+
+				
+				<div className="view-button-container">
+				<Button className={ view_button }> <img src={todayicon}></img>&nbsp;&nbsp;Today</Button><br/><br/>
+				<Button className={ view_button }> <img src={weeklyicon}></img>&nbsp;&nbsp;Weekly</Button><br/><br/>
+				<Button className={ view_button }> <img src={monthlyicon}></img>&nbsp;&nbsp;Monthly</Button><br/><br/>
+				<Button className={ view_button }> <img src={yearlyicon}></img>&nbsp;&nbsp;Yearly</Button><br/><br/>
+				</div>
+
+				<div className="mood-chart">
+					<div className="title"><h3>Mood Chart</h3><br/></div>
+					
+					<div className="mood-sq-container">
+						<div className="mood-happy mood-text"><p>Happy</p></div><br/>
+						<div className="mood-neutral mood-text">Neutral</div><br/>
+						<div className="mood-sad mood-text">Sad</div>
+
+					</div>
+
+				</div>
+			</div>			
 		);
 	}
 }
 
-export default SideBar;
+export default withStyles(styles)(SideBar);
